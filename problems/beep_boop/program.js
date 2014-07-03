e88f12f16b3485763b96cd33ae499a58f4404dd1
rajeshwerkushwaha <rajeshwerkushwaha@gmail.com>
2014-07-03 17:04:51 +0000

    solution by me

---
+++ problems/beep_boop/program.js
@@ -0,0 +1,8 @@
+/**
+* Created with stream-adventure.
+* User: rajeshwerkushwaha
+* Date: 2014-07-03
+* Time: 07:00 AM
+* To change this template use Tools | Templates.
+*/
+console.log('beep boop');
