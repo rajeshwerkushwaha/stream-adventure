15879e5f8d38e452c57effd578a44e659597dfc0
rajeshwerkushwaha <rajeshwerkushwaha@gmail.com>
2014-07-03 17:07:05 +0000

    solution by me

---
+++ problems/input_output/program.js
@@ -0,0 +1,8 @@
+/**
+* Created with stream-adventure.
+* User: rajeshwerkushwaha
+* Date: 2014-07-03
+* Time: 07:26 AM
+* To change this template use Tools | Templates.
+*/
+process.stdin.pipe(process.stdout);
